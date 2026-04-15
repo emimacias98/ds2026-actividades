460080992f8cb3993480e84f96e647f37365f3ed
Emiliano <emilianomacias2005@gmail.com>
2026-04-15 08:27:26 -0300

    Ejercicio 1: Variables y operaciones

---
+++ ejercicios/c03-javascript/ej1-variables/app.js
@@ -0,0 +1,13 @@
+const nombre = "Emiliano"
+const edad = 21
+const materia = "Desarrollo de software"
+
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
+
+let contador = 0;
+
+contador = contador +1;
+contador = contador +1;
+contador = contador +1;
+
+console.log("Valor final del contador:", contador);
