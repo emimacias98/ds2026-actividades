ab510f1b79c5d119eeaeab2077e5b6e8d454fb12
Emiliano <emilianomacias2005@gmail.com>
2026-04-15 08:28:10 -0300

    Clasificacion de notas y dias de la semana

---
+++ ejercicios/c03-javascript/ej2-condicionales/app.js
@@ -0,0 +1,40 @@
+
+function clasificarNota(nota) {
+    if (nota < 4) {
+        return "Desaprobado";
+    } else if (nota >= 4 && nota <= 7) {
+        return "Aprobado";
+    } else {
+        return "Promocionado";
+    }
+}
+
+console.log(clasificarNota(2));
+console.log(clasificarNota(5));
+console.log(clasificarNota(9));
+
+function diaDeLaSemana(numero) {
+    switch (numero) {
+        case 1:
+            return "Lunes";
+        case 2:
+            return "Martes";
+        case 3:
+            return "Miércoles";
+        case 4:
+            return "Jueves";
+        case 5:
+            return "Viernes";
+        case 6:
+            return "Sábado - (fin de semana)";
+        case 7:
+            return "Domingo - (fin de semana)";
+        default:
+            return "Día inválido";
+    }
+}
+
+console.log(diaDeLaSemana(1)); 
+console.log(diaDeLaSemana(4)); 
+console.log(diaDeLaSemana(7)); 
+console.log(diaDeLaSemana(10)); 
